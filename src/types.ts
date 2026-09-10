d66921638b405c50a437874b27d17d0a28f1b85d
Ankit-2563 <ankit123bhavarthe@gmail.com>
2026-09-10 19:16:10 +0530

    Add RemoteApp and DockItem types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,19 @@
 export interface MacHost {
   name: string;
-  host: string;
+  host: string; // IP address
   port: number;
 }
 
 export interface PairedMac extends MacHost {
   token: string;
+}
+
+export interface RemoteApp {
+  name: string;
+  path: string;
+  bundleId: string;
+}
+
+export interface DockItem extends RemoteApp {
+  addedAt: number;
 }
